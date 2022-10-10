870fddc9adf8bcf00dc1cce72ed6ff50fbfd1d90
Israel Alencar <israel.franca@bradescoseguros.com.br>
2022-10-10 14:49:34 -0300

    estrutura inicial do projeto

---
+++ src/test/java/br/com/myfood/MyfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.myfood;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
