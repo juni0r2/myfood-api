67e80bc7dfbbdf7ce93c9110d5e6859485b35a55
Israel Alencar de França <israel.franca@mjv.net>
2023-05-08 16:41:35 -0400

    adicionando relacionamento restaurante e cozinha

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,6 @@
-insert into cozinha (nom_cozinha) values ('Tailandesa');
-insert into cozinha (nom_cozinha) values ('Indiana');
+insert into cozinha (id, nom_cozinha) values (1, 'Tailandesa');
+insert into cozinha (id, nom_cozinha) values (2, 'Indiana');
 
-insert into restaurante (nome, taxa_frete) values ('COCO BAMBU', '7.45');
-insert into restaurante (nome, taxa_frete) values ('OUTBACK', '9.87');
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('COCO BAMBU', '7.45', 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('OUTBACK', '9.87', 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('SAKAMOTO', '19.87', 2);
