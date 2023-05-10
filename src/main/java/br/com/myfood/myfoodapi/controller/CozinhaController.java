f5caaa5ae6f86d535266934bd9f40da130c455c1
Israel Alencar de França <israel.franca@mjv.net>
2023-05-10 16:51:28 -0400

    corrigir nome classe e adicionando singleton resource

--- src/main/java/br/com/myfood/myfoodapi/controller/CozinhaController.java
+++ src/main/java/br/com/myfood/myfoodapi/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class ConzinhaController {
     @GetMapping
     public List<Cozinha> lista(){
         return this.service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable Long id) {
+        return this.service.buscaPorId(id);
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,5 +5,5 @@ insert into restaurante (nome, taxa_frete, cozinha_id) values ('COCO BAMBU', '7.
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('OUTBACK', '9.87', 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('SAKAMOTO', '19.87', 2);
 
-insert into estado (nome) values ('SaoPaulo');
+insert into estado (nome) values ('Sao Paulo');
 insert into estado (nome) values ('Mato Grosso do Sul');
