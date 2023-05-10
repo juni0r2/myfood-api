ac587f4b9d33d3caab8f014a8dd5d0d223691352
Israel Alencar de França <israel.franca@mjv.net>
2023-05-10 16:22:49 -0400

    delete da classe de TesteController e adicionando o Controller Estado

---
+++ src/main/java/br/com/myfood/myfoodapi/controller/EstadoController.java
@@ -0,0 +1,24 @@
+package br.com.myfood.myfoodapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.myfood.myfoodapi.domain.model.Estado;
+import br.com.myfood.myfoodapi.domain.repository.EstadoRepository;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+    
+    @Autowired
+    private EstadoRepository repository;
+
+    @GetMapping
+    public List<Estado> listar() {
+        return this.repository.findAll();
+    }
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,6 @@ insert into cozinha (id, nom_cozinha) values (2, 'Indiana');
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('COCO BAMBU', '7.45', 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('OUTBACK', '9.87', 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('SAKAMOTO', '19.87', 2);
+
+insert into estado (nome) values ('SaoPaulo');
+insert into estado (nome) values ('Mato Grosso do Sul');
