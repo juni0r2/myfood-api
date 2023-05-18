8e93c75da8dd69d705d9bb0003d0510b51a7b475
Israel Alencar de França <israel.franca@mjv.net>
2023-05-18 16:51:40 -0400

    criado recurso de criar cozinha

--- src/main/java/br/com/myfood/myfoodapi/api/controller/CozinhaController.java
+++ src/main/java/br/com/myfood/myfoodapi/api/controller/CozinhaController.java
@@ -3,10 +3,15 @@ package br.com.myfood.myfoodapi.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.myfood.myfoodapi.api.model.CozinhaXmlWrapper;
@@ -31,7 +36,29 @@ public class CozinhaController {
     }
 
     @GetMapping("/{id}")
-    public Cozinha buscar(@PathVariable Long id) {
-        return this.service.buscaPorId(id);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
+        Cozinha cozinha = this.service.buscaPorId(id);
+
+        // return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+        
+        // HttpHeaders headers = new HttpHeaders();
+        // headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+        
+        // return ResponseEntity
+        //         .status(HttpStatus.FOUND)
+        //         .headers(headers)
+        //         .build();
+        
+        if (cozinha == null) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        return ResponseEntity.ok(cozinha);
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+        return this.service.salvar(cozinha);
     }
 }
