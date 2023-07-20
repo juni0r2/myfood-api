de9447129b313f3ce34db80ca910864a4a248258
Israel Alencar de França <alenc4r25@gmail.com>
2023-07-20 18:44:50 -0400

    adicionado novo handle para mensagens incompreensiveis

--- src/main/java/br/com/myfood/myfoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/myfood/myfoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -6,6 +6,7 @@ import br.com.myfood.myfoodapi.domain.exception.NegocioException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -15,6 +16,17 @@ import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException e, HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest webRequest) {
+        ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+        String detail = "O corpo da requisição está inválido. Verifique erro de sintaxe.";
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return this.handleExceptionInternal(e, problem, new HttpHeaders(), status, webRequest);
+    }
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e, WebRequest webRequest) {
@@ -37,7 +49,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         Problem problem = createProblemBuilder(status, problemType, detail).build();
 
-        return this.handleExceptionInternal(e, problem,  new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
+        return this.handleExceptionInternal(e, problem, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
     }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
@@ -53,30 +65,19 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @Override
-    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
-                                                             HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
         if (body == null) {
-            body = Problem.builder()
-                    .status(status.value())
-                    .detail(ex.getMessage())
-                    .build();
+            body = Problem.builder().status(status.value()).detail(status.getReasonPhrase()).build();
         } else if (body instanceof String) {
-            body = Problem.builder()
-                    .status(status.value())
-                    .detail(ex.getMessage())
-                    .build();
+            body = Problem.builder().status(status.value()).detail(ex.getMessage()).build();
         }
 
         return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 
     private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
-        return Problem.builder()
-                .status(status.value())
-                .type(problemType.getUri())
-                .title(problemType.getTitle())
-                .detail(detail);
+        return Problem.builder().status(status.value()).type(problemType.getUri()).title(problemType.getTitle()).detail(detail);
     }
 
 }
