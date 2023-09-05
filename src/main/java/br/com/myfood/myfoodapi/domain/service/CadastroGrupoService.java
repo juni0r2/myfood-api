39e91b4750ef061e948652b1a5327e126bdf962e
Israel Alencar de França <alenc4r25@gmail.com>
2023-09-05 16:39:27 -0400

    ajuste para transacionar crud grupo

--- src/main/java/br/com/myfood/myfoodapi/domain/service/CadastroGrupoService.java
+++ src/main/java/br/com/myfood/myfoodapi/domain/service/CadastroGrupoService.java
@@ -1,15 +1,22 @@
 package br.com.myfood.myfoodapi.domain.service;
 
+import br.com.myfood.myfoodapi.domain.exception.EntidadeEmUsoException;
+import br.com.myfood.myfoodapi.domain.exception.EntidadeNaoEncontradaException;
 import br.com.myfood.myfoodapi.domain.exception.GrupoNaoEncontradoException;
 import br.com.myfood.myfoodapi.domain.model.Grupo;
 import br.com.myfood.myfoodapi.domain.repository.GrupoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class CadastroGrupoService {
+    private static final String MSG_GRUPO_EM_USO
+            = "Grupo de código %d não pode ser removido, pois está em uso";
 
     @Autowired
     private GrupoRepository grupoRepository;
@@ -23,12 +30,20 @@ public class CadastroGrupoService {
         return this.grupoRepository.findAll();
     }
 
+    @Transactional
     public Grupo salva(Grupo grupo) {
         return this.grupoRepository.save(grupo);
     }
 
+    @Transactional
     public void remove(Long id) {
-        Grupo grupo = this.buscarPorId(id);
-        this.grupoRepository.delete(grupo);
+        try {
+            this.grupoRepository.deleteById(id);
+            this.grupoRepository.flush();
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(String.format(MSG_GRUPO_EM_USO, id));
+        } catch (EmptyResultDataAccessException e) {
+            throw new GrupoNaoEncontradoException(id);
+        }
     }
 }
