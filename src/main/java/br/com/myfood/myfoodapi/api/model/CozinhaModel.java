f7a50ea5848cc55cfa1f299c86363064e8045503
Israel Alencar de França <alenc4r25@gmail.com>
2023-09-14 15:32:59 -0400

    feat:utilizando JsonView em representação de restaurantes

--- src/main/java/br/com/myfood/myfoodapi/api/model/CozinhaModel.java
+++ src/main/java/br/com/myfood/myfoodapi/api/model/CozinhaModel.java
@@ -1,5 +1,7 @@
 package br.com.myfood.myfoodapi.api.model;
 
+import br.com.myfood.myfoodapi.api.model.view.RestauranteView;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +9,8 @@ import lombok.Setter;
 @Setter
 public class CozinhaModel {
 
+    @JsonView({RestauranteView.Resumo.class})
     private Long id;
+    @JsonView({RestauranteView.Resumo.class})
     private String nome;
 }
---
+++ src/main/java/br/com/myfood/myfoodapi/api/model/view/RestauranteView.java
@@ -0,0 +1,8 @@
+package br.com.myfood.myfoodapi.api.model.view;
+
+public interface RestauranteView {
+
+    public interface Resumo {}
+
+    public interface ApenasNome {}
+}
