92aae28108c48203b75d988fd06ecea77173b00d
Israel Alencar de França <alenc4r25@gmail.com>
2023-11-06 19:53:57 -0400

    feat:criando migration e mapeando entidade foto produto

---
+++ src/main/java/br/com/myfood/myfoodapi/domain/model/FotoProduto.java
@@ -0,0 +1,30 @@
+package br.com.myfood.myfoodapi.domain.model;
+
+import br.com.myfood.myfoodapi.core.validation.Groups;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+public class FotoProduto {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name="produto_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private Produto produto;
+
+    @NotBlank
+    private String nomeArquivo;
+    private String descricao;
+    @NotBlank
+    private String contentType;
+    private Long tamanho;
+}
